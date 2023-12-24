c8ce89347b0d3ae23fcd652481be21353b0369eb
OnagaHaruki <haloupgarou@gmail.com>
2023-12-24 23:16:33 +0900

    XCTContext.runActivity()に'try'を付与することでテストが実行できるようになった

--- XcodeTestSample/XcodeTestSampleTests/ThrowingErrorTest.swift
+++ XcodeTestSample/XcodeTestSampleTests/ThrowingErrorTest.swift
@@ -16,22 +16,27 @@ func validateWithThrowingError(password: String) throws {
 
 final class ThrowingErrorTest: XCTestCase {
     func test_パスワードが10文字以上であること() {
-        XCTAssertThrowsError(try validateWithThrowingError(password: "123abcdef")) { error in
-            XCTAssertEqual(error as? ValidationError, ValidationError.notMoreThan10Characters)
-        }
-
         XCTContext.runActivity(named: "数字が3文字含まれている") { _ in
-            XCTContext.runActivity(named: "合計9文字入力された場合にnotMoreThan10Charactersを返すこと") { _ in
-                XCTAssertThrowsError(try validateWithThrowingError(password: "123abcdef")) { error in
-                    XCTAssertEqual(error as? ValidationError, ValidationError.notMoreThan10Characters)
+            do {
+                // XCTContext.runActivityはrethrowsなメソッド(引数で受け取った処理のエラーを受け取る)
+                // XCTAssertThrowsError()はthrowsなメソッドではないので，XCTContext.runActivity()に'try'は必要ないはず
+                // コンパイルエラーを防ぐためとか，上手く解析ができないとかで仕方なく'try'をつけている感じ?
+                try XCTContext.runActivity(named: "合計9文字入力された場合にnotMoreThan10Charactersを返すこと") { _ in
+                    XCTAssertThrowsError(try validateWithThrowingError(password: "123abcdef")) { error in
+                        XCTAssertEqual(error as? ValidationError, ValidationError.notMoreThan10Characters)
+                        print("In XCTAssertEqual \(error)")
+                    }
                 }
+            } catch {
+                // XCTAssertThrowsError()に渡したvalidateWithThrowingError()はエラーをthrowするが，このcatch節には流れてこない
+                print(error)
             }
 
-            XCTContext.runActivity(named: "合計10文字入力された場合に何も返さないこと") { _ in
+            try? XCTContext.runActivity(named: "合計10文字入力された場合に何も返さないこと") { _ in
                 XCTAssertNoThrow(try validateWithThrowingError(password: "123abcdefg"))
             }
 
-            XCTContext.runActivity(named: "合計11文字入力された場合に何も返さないこと") { _ in
+            try? XCTContext.runActivity(named: "合計11文字入力された場合に何も返さないこと") { _ in
                 XCTAssertNoThrow(try validateWithThrowingError(password: "123abcdefgh"))
             }
         }
@@ -39,15 +44,15 @@ final class ThrowingErrorTest: XCTestCase {
 
     func test_パスワードが数字を3文字以上含むこと() {
         XCTContext.runActivity(named: "数字以外が9文字入力されている") { _ in
-            XCTContext.runActivity(named: "数字が2文字入力された場合にdoesNotContainMoreThan3Numeralsを返すこと") { _ in
+            try? XCTContext.runActivity(named: "数字が2文字入力された場合にdoesNotContainMoreThan3Numeralsを返すこと") { _ in
                 XCTAssertThrowsError(try validateWithThrowingError(password: "abcdefghi12"))
             }
 
-            XCTContext.runActivity(named: "数字が3文字入力された場合にtrueを返すこと") { _ in
+            try? XCTContext.runActivity(named: "数字が3文字入力された場合にtrueを返すこと") { _ in
                 XCTAssertNoThrow(try validateWithThrowingError(password: "abcdefghi123"))
             }
 
-            XCTContext.runActivity(named: "数字が4文字入力された場合にtrueを返すこと") { _ in
+            try? XCTContext.runActivity(named: "数字が4文字入力された場合にtrueを返すこと") { _ in
                 XCTAssertNoThrow(try validateWithThrowingError(password: "abcdefghi1234"))
             }
         }
